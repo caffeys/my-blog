247dc95f7b59ef3f02f33d63b6b1f88a14fcc41f
Sean Caffey <spcaffey@yahoo.com>
2019-10-18 15:34:45 -0700

    first commit - removed node_modules

---
+++ src/build/precache-manifest.aae9f8a123d9b5392c382d229e07873f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41f95fca205c6fa7a1410886eb8a9927",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2875159e73bf92b3c904",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "65525d98e6835d505d2f",
+    "url": "/static/js/2.f5e6359f.chunk.js"
+  },
+  {
+    "revision": "2875159e73bf92b3c904",
+    "url": "/static/js/main.68e16d99.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
